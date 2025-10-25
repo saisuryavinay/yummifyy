accde27febb98d0f3c3b241291c82fe200f89e12
saisuryavinay <saisuryavinay06@gmail.com>
2025-10-25 12:10:19 +0530

    first full stack project

--- testing/testing/src/App.jsx
+++ testing/testing/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import bujji from './assets/bajji.webp';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <img src={bujji} alt='hello' />
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn morexkcdkndskl
       </p>
     </>
   )
